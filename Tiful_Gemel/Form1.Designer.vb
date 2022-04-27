4abe119e34754df90394f12b82e40e1a62d7b742
Ilan <44946919+ilansh7@users.noreply.github.com>
2022-04-28 01:48:55 +0300

    Add files via upload

---
+++ Tiful_Gemel/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblAmount = New System.Windows.Forms.Label()
+        Me.lblDepositDate = New System.Windows.Forms.Label()
+        Me.lblBirthDay = New System.Windows.Forms.Label()
+        Me.lblIDNumber = New System.Windows.Forms.Label()
+        Me.dtBitrhDay = New System.Windows.Forms.DateTimePicker()
+        Me.dtDepositDate = New System.Windows.Forms.DateTimePicker()
+        Me.txtIDNumber = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.btnApplyDeposit = New System.Windows.Forms.Button()
+        Me.dgTiful = New System.Windows.Forms.DataGridView()
+        Me.btnViewDeposit = New System.Windows.Forms.Button()
+        Me.btnClearForm = New System.Windows.Forms.Button()
+        Me.btnUpdateDeposit = New System.Windows.Forms.Button()
+        Me.txtDepositID = New System.Windows.Forms.TextBox()
+        Me.lblDepositId = New System.Windows.Forms.Label()
+        Me.btnDeleteDeposit = New System.Windows.Forms.Button()
+        CType(Me.dgTiful, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Calibri", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(177, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblTitle.Location = New System.Drawing.Point(271, 34)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(158, 42)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "תפעול גמל"
+        '
+        'lblAmount
+        '
+        Me.lblAmount.AutoSize = True
+        Me.lblAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAmount.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblAmount.Location = New System.Drawing.Point(33, 258)
+        Me.lblAmount.Name = "lblAmount"
+        Me.lblAmount.Size = New System.Drawing.Size(65, 20)
+        Me.lblAmount.TabIndex = 2
+        Me.lblAmount.Text = "Amount"
+        '
+        'lblDepositDate
+        '
+        Me.lblDepositDate.AutoSize = True
+        Me.lblDepositDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDepositDate.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblDepositDate.Location = New System.Drawing.Point(33, 220)
+        Me.lblDepositDate.Name = "lblDepositDate"
+        Me.lblDepositDate.Size = New System.Drawing.Size(103, 20)
+        Me.lblDepositDate.TabIndex = 3
+        Me.lblDepositDate.Text = "Deposit Date"
+        '
+        'lblBirthDay
+        '
+        Me.lblBirthDay.AutoSize = True
+        Me.lblBirthDay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBirthDay.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblBirthDay.Location = New System.Drawing.Point(33, 182)
+        Me.lblBirthDay.Name = "lblBirthDay"
+        Me.lblBirthDay.Size = New System.Drawing.Size(74, 20)
+        Me.lblBirthDay.TabIndex = 4
+        Me.lblBirthDay.Text = "Birth Day"
+        '
+        'lblIDNumber
+        '
+        Me.lblIDNumber.AutoSize = True
+        Me.lblIDNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIDNumber.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblIDNumber.Location = New System.Drawing.Point(33, 144)
+        Me.lblIDNumber.Name = "lblIDNumber"
+        Me.lblIDNumber.Size = New System.Drawing.Size(86, 20)
+        Me.lblIDNumber.TabIndex = 5
+        Me.lblIDNumber.Text = "ID Number"
+        '
+        'dtBitrhDay
+        '
+        Me.dtBitrhDay.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtBitrhDay.CustomFormat = " "
+        Me.dtBitrhDay.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtBitrhDay.Location = New System.Drawing.Point(198, 182)
+        Me.dtBitrhDay.Name = "dtBitrhDay"
+        Me.dtBitrhDay.Size = New System.Drawing.Size(200, 20)
+        Me.dtBitrhDay.TabIndex = 8
+        '
+        'dtDepositDate
+        '
+        Me.dtDepositDate.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtDepositDate.CustomFormat = " "
+        Me.dtDepositDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtDepositDate.Location = New System.Drawing.Point(198, 220)
+        Me.dtDepositDate.Name = "dtDepositDate"
+        Me.dtDepositDate.Size = New System.Drawing.Size(200, 20)
+        Me.dtDepositDate.TabIndex = 9
+        '
+        'txtIDNumber
+        '
+        Me.txtIDNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIDNumber.Location = New System.Drawing.Point(198, 143)
+        Me.txtIDNumber.Name = "txtIDNumber"
+        Me.txtIDNumber.Size = New System.Drawing.Size(200, 22)
+        Me.txtIDNumber.TabIndex = 11
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAmount.Location = New System.Drawing.Point(198, 257)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(200, 22)
+        Me.txtAmount.TabIndex = 12
+        '
+        'btnApplyDeposit
+        '
+        Me.btnApplyDeposit.BackColor = System.Drawing.Color.Silver
+        Me.btnApplyDeposit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnApplyDeposit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnApplyDeposit.Location = New System.Drawing.Point(521, 149)
+        Me.btnApplyDeposit.Name = "btnApplyDeposit"
+        Me.btnApplyDeposit.Size = New System.Drawing.Size(146, 37)
+        Me.btnApplyDeposit.TabIndex = 13
+        Me.btnApplyDeposit.Text = "Apply Deposit"
+        Me.btnApplyDeposit.UseVisualStyleBackColor = False
+        '
+        'dgTiful
+        '
+        Me.dgTiful.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgTiful.Location = New System.Drawing.Point(33, 315)
+        Me.dgTiful.Name = "dgTiful"
+        Me.dgTiful.ReadOnly = True
+        Me.dgTiful.Size = New System.Drawing.Size(634, 193)
+        Me.dgTiful.TabIndex = 14
+        '
+        'btnViewDeposit
+        '
+        Me.btnViewDeposit.BackColor = System.Drawing.Color.Silver
+        Me.btnViewDeposit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnViewDeposit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnViewDeposit.Location = New System.Drawing.Point(521, 98)
+        Me.btnViewDeposit.Name = "btnViewDeposit"
+        Me.btnViewDeposit.Size = New System.Drawing.Size(146, 37)
+        Me.btnViewDeposit.TabIndex = 15
+        Me.btnViewDeposit.Text = "View Deposits"
+        Me.btnViewDeposit.UseVisualStyleBackColor = False
+        '
+        'btnClearForm
+        '
+        Me.btnClearForm.BackColor = System.Drawing.Color.Silver
+        Me.btnClearForm.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClearForm.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnClearForm.Location = New System.Drawing.Point(281, 527)
+        Me.btnClearForm.Name = "btnClearForm"
+        Me.btnClearForm.Size = New System.Drawing.Size(139, 37)
+        Me.btnClearForm.TabIndex = 16
+        Me.btnClearForm.Text = "Clear"
+        Me.btnClearForm.UseVisualStyleBackColor = False
+        '
+        'btnUpdateDeposit
+        '
+        Me.btnUpdateDeposit.BackColor = System.Drawing.Color.Silver
+        Me.btnUpdateDeposit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdateDeposit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnUpdateDeposit.Location = New System.Drawing.Point(521, 200)
+        Me.btnUpdateDeposit.Name = "btnUpdateDeposit"
+        Me.btnUpdateDeposit.Size = New System.Drawing.Size(146, 37)
+        Me.btnUpdateDeposit.TabIndex = 17
+        Me.btnUpdateDeposit.Text = "Update Deposits"
+        Me.btnUpdateDeposit.UseVisualStyleBackColor = False
+        '
+        'txtDepositID
+        '
+        Me.txtDepositID.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDepositID.Location = New System.Drawing.Point(198, 105)
+        Me.txtDepositID.Name = "txtDepositID"
+        Me.txtDepositID.Size = New System.Drawing.Size(88, 22)
+        Me.txtDepositID.TabIndex = 19
+        '
+        'lblDepositId
+        '
+        Me.lblDepositId.AutoSize = True
+        Me.lblDepositId.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDepositId.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lblDepositId.Location = New System.Drawing.Point(33, 106)
+        Me.lblDepositId.Name = "lblDepositId"
+        Me.lblDepositId.Size = New System.Drawing.Size(85, 20)
+        Me.lblDepositId.TabIndex = 18
+        Me.lblDepositId.Text = "Deposit ID"
+        '
+        'btnDeleteDeposit
+        '
+        Me.btnDeleteDeposit.BackColor = System.Drawing.Color.Silver
+        Me.btnDeleteDeposit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDeleteDeposit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnDeleteDeposit.Location = New System.Drawing.Point(521, 251)
+        Me.btnDeleteDeposit.Name = "btnDeleteDeposit"
+        Me.btnDeleteDeposit.Size = New System.Drawing.Size(146, 37)
+        Me.btnDeleteDeposit.TabIndex = 20
+        Me.btnDeleteDeposit.Text = "Delete Deposits"
+        Me.btnDeleteDeposit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(745, 600)
+        Me.Controls.Add(Me.btnDeleteDeposit)
+        Me.Controls.Add(Me.txtDepositID)
+        Me.Controls.Add(Me.lblDepositId)
+        Me.Controls.Add(Me.btnUpdateDeposit)
+        Me.Controls.Add(Me.btnClearForm)
+        Me.Controls.Add(Me.btnViewDeposit)
+        Me.Controls.Add(Me.dgTiful)
+        Me.Controls.Add(Me.btnApplyDeposit)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.txtIDNumber)
+        Me.Controls.Add(Me.dtDepositDate)
+        Me.Controls.Add(Me.dtBitrhDay)
+        Me.Controls.Add(Me.lblIDNumber)
+        Me.Controls.Add(Me.lblBirthDay)
+        Me.Controls.Add(Me.lblDepositDate)
+        Me.Controls.Add(Me.lblAmount)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.RightToLeftLayout = True
+        Me.Text = "מבחן בית - אילן שחורי"
+        CType(Me.dgTiful, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblAmount As Label
+    Friend WithEvents lblDepositDate As Label
+    Friend WithEvents lblBirthDay As Label
+    Friend WithEvents lblIDNumber As Label
+    Friend WithEvents dtBitrhDay As DateTimePicker
+    Friend WithEvents dtDepositDate As DateTimePicker
+    Friend WithEvents txtIDNumber As TextBox
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents btnApplyDeposit As Button
+    Friend WithEvents dgTiful As DataGridView
+    Friend WithEvents btnViewDeposit As Button
+    Friend WithEvents btnClearForm As Button
+    Friend WithEvents btnUpdateDeposit As Button
+    Friend WithEvents txtDepositID As TextBox
+    Friend WithEvents lblDepositId As Label
+    Friend WithEvents btnDeleteDeposit As Button
+End Class
---
+++ Tiful_Gemel/Form1.vb
@@ -0,0 +1,184 @@
+﻿'Option Strict On
+'Option Explicit On
+
+Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1 : Implements IDisposable
+    Dim conn As New SqlConnection("Data Source=.;Initial Catalog=TifulGemel;Integrated Security=True")
+    Dim formIsValid As Boolean = True
+    Dim errMsg As String = ""
+
+    Private Sub btnApplyDeposit_Click(sender As Object, e As EventArgs) Handles btnApplyDeposit.Click
+        Dim CustomerId As String = txtIDNumber.Text
+        Dim CustomerBirthDay As DateTime = dtBitrhDay.Value
+        Dim DepositDate As DateTime = dtDepositDate.Value
+        Dim Amount As Decimal = 0 'txtAmount.Text
+
+        ' Validations
+        Dim dblAmount As Double = 0.0
+        If Double.TryParse(txtAmount.Text, dblAmount) Then
+            Amount = dblAmount
+        Else
+            errMsg = "הסכום אינו תקין."
+            formIsValid = False
+        End If
+        If dtDepositDate.Text = " " Then
+            errMsg = "שדה תאריך הפקדה הוא חובה."
+            formIsValid = False
+        End If
+        If dtBitrhDay.Text = " " Then
+            errMsg = "שדה תאריך לידה הוא חובה."
+            formIsValid = False
+        End If
+        If String.IsNullOrEmpty(txtIDNumber.Text) Then
+            errMsg = "שדה מזהה לקוח הוא חובה."
+            formIsValid = False
+        End If
+        If formIsValid Then
+            conn.Open()
+            Dim cmd As New SqlCommand("insert into [dbo].[Deposits] values ('" & CustomerId & "','" & CustomerBirthDay & "','" & DepositDate & "','" & Amount & "')", conn)
+            cmd.ExecuteNonQuery()
+            'MessageBox.Show("רשומה התווספה בהצלחה.")
+            conn.Close()
+            GetDepositDetails(False)
+        Else
+            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
+        End If
+        conn.Close()
+
+        'StatusStrip1.Text = ""
+
+        'conn.Dispose()
+        'Dispose()
+
+    End Sub
+    Private Sub GetDepositDetails(CreteriaByFields As Boolean)
+        ' the parameter is for reload data after add/modify without using parameters for select statement.
+        conn.Open()
+        Dim sqlCommand As String = "select * from [dbo].[Deposits]"
+        If CreteriaByFields Then
+            Dim strAnd = "", strWhere = ""
+            If Not String.IsNullOrEmpty(txtIDNumber.Text) Then
+                strWhere = "where"
+                sqlCommand += " " + strWhere + " " + strAnd + " " + "IDNumber = '" & txtIDNumber.Text & "'"
+            End If
+            If Not String.IsNullOrEmpty((dtBitrhDay.Value).ToString) And Not dtBitrhDay.Text = " " Then
+                If String.IsNullOrEmpty(strWhere) Then
+                    strWhere = "where"
+                Else
+                    strAnd = "and"
+                    strWhere = ""
+                End If
+                sqlCommand += " " + strWhere + " " + strAnd + " " + "CONVERT(VARCHAR(10), BirthDay, 103) = '" & dtBitrhDay.Value.ToString("dd/MM/yyyy") & "'"
+            End If
+            If Not String.IsNullOrEmpty((dtDepositDate.Value).ToString) And Not dtDepositDate.Text = " " Then
+                If String.IsNullOrEmpty(strWhere) Then
+                    strWhere = "where"
+                Else
+                    strAnd = "and"
+                    strWhere = ""
+                End If
+                sqlCommand += " " + strWhere + " " + strAnd + " " + "CONVERT(VARCHAR(10), DepositDate, 103) = '" & dtDepositDate.Value.ToString("dd/MM/yyyy") & "'" 'ToShortDateString & "'"
+            End If
+        End If
+        sqlCommand += " order by DepositDate desc"
+        Dim cmd As New SqlCommand(sqlCommand, conn)
+        Dim sqlDA As New SqlDataAdapter(cmd)
+        Dim dt As New DataTable()
+        sqlDA.Fill(dt)
+        Dim i As Integer = dt.Rows.Count
+        Dim amount As Decimal
+        Dim birthDay As DateTime
+        For i = 0 To dt.Rows.Count - 1 Step 1
+            'birthDay = Convert.ToDateTime(dt.Rows(i).Item(2))
+            birthDay = dt.Rows(i).Item(2)
+            amount = dt.Rows(i).Item(4)
+            'dt.Rows(i).Item(2) = birthDay.ToString("dd/MM/yyyy")
+            dt.Rows(i).Item(2) = birthDay.ToShortDateString()
+            dt.Rows(i).Item(4) = amount.ToString("N2")
+        Next
+        dgTiful.DataSource = dt
+        formIsValid = True
+
+        conn.Close()
+    End Sub
+
+    Private Sub btnViewDeposit_Click(sender As Object, e As EventArgs) Handles btnViewDeposit.Click
+        GetDepositDetails(True)
+    End Sub
+
+    Private Sub btnClearForm_Click(sender As Object, e As EventArgs) Handles btnClearForm.Click
+        txtDepositID.Text = ""
+        txtIDNumber.Text = ""
+        dtBitrhDay.CustomFormat = " "  'An empty SPACE
+        dtBitrhDay.Format = DateTimePickerFormat.Custom
+        dtDepositDate.CustomFormat = " "  'An empty SPACE
+        dtDepositDate.Format = DateTimePickerFormat.Custom
+        txtAmount.Text = ""
+        dgTiful.DataSource = ""
+
+    End Sub
+
+    Private Sub dtDepositDate_ValueChanged(sender As Object, e As EventArgs) Handles dtDepositDate.ValueChanged
+        dtDepositDate.CustomFormat = "dd/MM/yyyy"
+    End Sub
+
+    Private Sub dtBitrhDay_ValueChanged(sender As Object, e As EventArgs) Handles dtBitrhDay.ValueChanged
+        dtBitrhDay.CustomFormat = "dd/MM/yyyy"
+    End Sub
+
+    Private Sub btnUpdateDeposit_Click(sender As Object, e As EventArgs) Handles btnUpdateDeposit.Click
+        Dim Amount As Decimal = 0
+        ' Validations
+        Dim dblAmount As Double = 0.0
+        If Double.TryParse(txtAmount.Text, dblAmount) Then
+            Amount = dblAmount
+        Else
+            errMsg = "הסכום אינו תקין."
+            formIsValid = False
+        End If
+        If String.IsNullOrEmpty(txtDepositID.Text) Then
+            errMsg = "שדה מזהה הפקדה הוא חובה."
+            formIsValid = False
+        End If
+
+        If formIsValid Then
+            conn.Open()
+            Dim cmd As New SqlCommand("update [dbo].[Deposits] set Amount = '" & Amount & "' where DepositId = " & txtDepositID.Text, conn)
+            cmd.ExecuteNonQuery()
+            'MessageBox.Show("רשומה התווספה בהצלחה.")
+            conn.Close()
+            GetDepositDetails(False)
+        Else
+            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
+        End If
+        conn.Close()
+
+        GetDepositDetails(False)
+    End Sub
+
+    Private Sub btnDeleteDeposit_Click(sender As Object, e As EventArgs) Handles btnDeleteDeposit.Click
+        ' Validations
+        If String.IsNullOrEmpty(txtDepositID.Text) Then
+            errMsg = "שדה מזהה הפקדה הוא חובה."
+            formIsValid = False
+        End If
+
+        If formIsValid Then
+            Dim result = MessageBox.Show("האם את/ה בטוח ?", "מחיקת הפקדה מס' " & txtDepositID.Text, MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                conn.Open()
+                Dim cmd As New SqlCommand("delete from [dbo].[Deposits] where DepositId = " & txtDepositID.Text, conn)
+                cmd.ExecuteNonQuery()
+                'MessageBox.Show("רשומה התווספה בהצלחה.")
+                conn.Close()
+                GetDepositDetails(False)
+            End If
+        Else
+            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
+        End If
+        conn.Close()
+        GetDepositDetails(False)
+    End Sub
+End Class
