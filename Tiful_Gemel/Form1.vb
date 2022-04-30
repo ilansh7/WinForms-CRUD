f5a163deb082b6cf4ae7308990db180fe9121972
Ilan <44946919+ilansh7@users.noreply.github.com>
2022-04-30 13:06:04 +0300

    Add files via upload

--- Tiful_Gemel/Form1.vb
+++ Tiful_Gemel/Form1.vb
@@ -4,64 +4,13 @@
 Imports System.Data.SqlClient
 Imports System.IO
 
+
 Public Class Form1 : Implements IDisposable
     Dim conn As New SqlConnection("Data Source=.;Initial Catalog=TifulGemel;Integrated Security=True")
     Dim formIsValid As Boolean = True
+    Dim clearDataGridFlag As Boolean = False
     Dim errMsg As String = ""
 
-    Private Sub btnApplyDeposit_Click(sender As Object, e As EventArgs) Handles btnApplyDeposit.Click
-        Dim CustomerId As String = txtIDNumber.Text
-        Dim CustomerBirthDay As DateTime = dtBitrhDay.Value
-        Dim DepositDate As DateTime = dtDepositDate.Value
-        Dim Amount As Decimal = 0 'txtAmount.Text
-
-        txtAmount.BackColor = System.Drawing.SystemColors.Window
-        dtDepositDate.CalendarMonthBackground = System.Drawing.SystemColors.Window
-        dtBitrhDay.CalendarMonthBackground = System.Drawing.SystemColors.Window
-        txtIDNumber.BackColor = System.Drawing.SystemColors.Window
-
-        ' Validations
-        Dim dblAmount As Double = 0.0
-        If Double.TryParse(txtAmount.Text, dblAmount) Then
-            Amount = dblAmount
-        Else
-            errMsg = "הסכום אינו תקין."
-            txtAmount.BackColor = Color.Pink
-            formIsValid = False
-        End If
-        If dtDepositDate.Text = " " Then
-            errMsg = "שדה תאריך הפקדה הוא חובה."
-            dtDepositDate.CalendarMonthBackground = Color.Pink
-            formIsValid = False
-        End If
-        If dtBitrhDay.Text = " " Then
-            errMsg = "שדה תאריך לידה הוא חובה."
-            dtBitrhDay.CalendarMonthBackground = Color.Pink
-            formIsValid = False
-        End If
-        If String.IsNullOrEmpty(txtIDNumber.Text) Then
-            errMsg = "שדה מזהה לקוח הוא חובה."
-            txtIDNumber.BackColor = Color.Pink
-            formIsValid = False
-        End If
-        If formIsValid Then
-            conn.Open()
-            Dim cmd As New SqlCommand("insert into [dbo].[Deposits] values ('" & CustomerId & "','" & CustomerBirthDay & "','" & DepositDate & "','" & Amount & "')", conn)
-            cmd.ExecuteNonQuery()
-            'MessageBox.Show("רשומה התווספה בהצלחה.")
-            conn.Close()
-            GetDepositDetails(False)
-        Else
-            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
-        End If
-        conn.Close()
-
-        'StatusStrip1.Text = ""
-
-        'conn.Dispose()
-        'Dispose()
-
-    End Sub
     Private Sub GetDepositDetails(CreteriaByFields As Boolean)
         ' the parameter is for reload data after add/modify without using parameters for select statement.
         conn.Open()
@@ -113,15 +62,113 @@ Public Class Form1 : Implements IDisposable
         conn.Close()
     End Sub
 
-    Private Sub btnViewDeposit_Click(sender As Object, e As EventArgs) Handles btnViewDeposit.Click
-        GetDepositDetails(True)
-    End Sub
+    Private Sub SetDeposit(Mode As String)
+        Dim CustomerId As String = txtIDNumber.Text
+        Dim CustomerBirthDay As DateTime = dtBitrhDay.Value
+        Dim DepositDate As DateTime = dtDepositDate.Value
+        Dim Amount As Decimal = 0 'txtAmount.Text
 
-    Private Sub btnClearForm_Click(sender As Object, e As EventArgs) Handles btnClearForm.Click
         txtAmount.BackColor = System.Drawing.SystemColors.Window
         dtDepositDate.CalendarMonthBackground = System.Drawing.SystemColors.Window
         dtBitrhDay.CalendarMonthBackground = System.Drawing.SystemColors.Window
         txtIDNumber.BackColor = System.Drawing.SystemColors.Window
+
+        formIsValid = True
+        errMsg = ""
+        Validation(Mode)
+
+        Dim cmd As New SqlCommand()
+        If formIsValid Then
+            Dim dblAmount As Double = 0.0
+            If Double.TryParse(txtAmount.Text, dblAmount) Then
+                Amount = dblAmount
+            End If
+
+            cmd.Connection = conn
+            Select Case Mode
+                Case "I"
+                    cmd.CommandText = "insert into [dbo].[Deposits] values (N'" & CustomerId & "','" & CustomerBirthDay & "','" & DepositDate & "','" & Amount & "')"
+                Case "U"
+                    cmd.CommandText = "update [dbo].[Deposits] set Amount = '" & Amount & "' where DepositId = " & txtDepositID.Text
+                Case "D"
+                    cmd.CommandText = "delete from [dbo].[Deposits] where DepositId = " & txtDepositID.Text
+                Case Else
+                    formIsValid = False
+                    errMsg = "סוג פעולה שנבחר אינו מוכר."
+            End Select
+        End If
+
+        If formIsValid Then
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            'MessageBox.Show("רשומה התווספה בהצלחה.")
+            conn.Close()
+            ClearForm()
+            GetDepositDetails(False)
+        Else
+            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
+            formIsValid = True
+        End If
+        'conn.Close()
+    End Sub
+
+    Private Sub Validation(Mode As String)
+        Dim Amount As Decimal = 0 'txtAmount.Text
+
+        txtAmount.BackColor = System.Drawing.SystemColors.Window
+        dtDepositDate.CalendarMonthBackground = System.Drawing.SystemColors.Window
+        dtBitrhDay.CalendarMonthBackground = System.Drawing.SystemColors.Window
+        txtIDNumber.BackColor = System.Drawing.SystemColors.Window
+
+        formIsValid = True
+        ' Validations
+        Dim dblAmount As Double = 0.0
+        If Mode = "U" Or Mode = "D" Then
+            If String.IsNullOrEmpty(txtDepositID.Text) Then
+                errMsg = "שדה מזהה הפקדה הוא חובה."
+                txtDepositID.BackColor = Color.Pink
+                formIsValid = False
+                Exit Sub
+            End If
+        End If
+        If Mode = "I" Then
+            If String.IsNullOrEmpty(txtIDNumber.Text) Then
+                errMsg = "שדה מזהה לקוח הוא חובה."
+                txtIDNumber.BackColor = Color.Pink
+                formIsValid = False
+                Exit Sub
+            End If
+            If dtBitrhDay.Text = " " Then
+                errMsg = "שדה תאריך לידה הוא חובה."
+                dtBitrhDay.CalendarMonthBackground = Color.Pink
+                formIsValid = False
+                Exit Sub
+            End If
+            If dtDepositDate.Text = " " Then
+                errMsg = "שדה תאריך הפקדה הוא חובה."
+                dtDepositDate.CalendarMonthBackground = Color.Pink
+                formIsValid = False
+                Exit Sub
+            End If
+        End If
+        If Mode = "I" Or Mode = "D" Then
+            If Double.TryParse(txtAmount.Text, dblAmount) Then
+                Amount = dblAmount
+            Else
+                errMsg = "הסכום אינו תקין."
+                txtAmount.BackColor = Color.Pink
+                formIsValid = False
+                Exit Sub
+            End If
+        End If
+    End Sub
+
+    Private Sub ClearForm()
+        txtAmount.BackColor = System.Drawing.SystemColors.Window
+        dtDepositDate.CalendarMonthBackground = System.Drawing.SystemColors.Window
+        dtBitrhDay.CalendarMonthBackground = System.Drawing.SystemColors.Window
+        txtIDNumber.BackColor = System.Drawing.SystemColors.Window
+        txtDepositID.BackColor = System.Drawing.SystemColors.Window
         txtDepositID.Text = ""
         txtIDNumber.Text = ""
         dtBitrhDay.CustomFormat = " "  'An empty SPACE
@@ -129,8 +176,35 @@ Public Class Form1 : Implements IDisposable
         dtDepositDate.CustomFormat = " "  'An empty SPACE
         dtDepositDate.Format = DateTimePickerFormat.Custom
         txtAmount.Text = ""
-        dgTiful.DataSource = ""
+        If clearDataGridFlag Then
+            dgTiful.DataSource = ""
+        End If
+        clearDataGridFlag = False
+        'MessageBox.Show("hey you")
+    End Sub
 
+    Private Sub btnViewDeposit_Click(sender As Object, e As EventArgs) Handles btnViewDeposit.Click
+        GetDepositDetails(True)
+    End Sub
+
+    Private Sub btnApplyDeposit_Click(sender As Object, e As EventArgs) Handles btnApplyDeposit.Click
+        SetDeposit("I")
+    End Sub
+
+    Private Sub btnUpdateDeposit_Click(sender As Object, e As EventArgs) Handles btnUpdateDeposit.Click
+        SetDeposit("U")
+    End Sub
+
+    Private Sub btnDeleteDeposit_Click(sender As Object, e As EventArgs) Handles btnDeleteDeposit.Click
+        Dim result = MessageBox.Show("האם את/ה בטוח ?", "מחיקת הפקדה מס' " & txtDepositID.Text, MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            SetDeposit("D")
+        End If
+    End Sub
+
+    Private Sub btnClearForm_Click(sender As Object, e As EventArgs) Handles btnClearForm.Click
+        clearDataGridFlag = True
+        ClearForm()
     End Sub
 
     Private Sub dtDepositDate_ValueChanged(sender As Object, e As EventArgs) Handles dtDepositDate.ValueChanged
@@ -140,61 +214,6 @@ Public Class Form1 : Implements IDisposable
     Private Sub dtBitrhDay_ValueChanged(sender As Object, e As EventArgs) Handles dtBitrhDay.ValueChanged
         dtBitrhDay.CustomFormat = "dd/MM/yyyy"
     End Sub
-
-    Private Sub btnUpdateDeposit_Click(sender As Object, e As EventArgs) Handles btnUpdateDeposit.Click
-        Dim Amount As Decimal = 0
-        ' Validations
-        Dim dblAmount As Double = 0.0
-        If Double.TryParse(txtAmount.Text, dblAmount) Then
-            Amount = dblAmount
-        Else
-            errMsg = "הסכום אינו תקין."
-            formIsValid = False
-        End If
-        If String.IsNullOrEmpty(txtDepositID.Text) Then
-            errMsg = "שדה מזהה הפקדה הוא חובה."
-            formIsValid = False
-        End If
-
-        If formIsValid Then
-            conn.Open()
-            Dim cmd As New SqlCommand("update [dbo].[Deposits] set Amount = '" & Amount & "' where DepositId = " & txtDepositID.Text, conn)
-            cmd.ExecuteNonQuery()
-            'MessageBox.Show("רשומה התווספה בהצלחה.")
-            conn.Close()
-            GetDepositDetails(False)
-        Else
-            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
-        End If
-        conn.Close()
-
-        GetDepositDetails(False)
-    End Sub
-
-    Private Sub btnDeleteDeposit_Click(sender As Object, e As EventArgs) Handles btnDeleteDeposit.Click
-        ' Validations
-        If String.IsNullOrEmpty(txtDepositID.Text) Then
-            errMsg = "שדה מזהה הפקדה הוא חובה."
-            formIsValid = False
-        End If
-
-        If formIsValid Then
-            Dim result = MessageBox.Show("האם את/ה בטוח ?", "מחיקת הפקדה מס' " & txtDepositID.Text, MessageBoxButtons.YesNo)
-            If result = DialogResult.Yes Then
-                conn.Open()
-                Dim cmd As New SqlCommand("delete from [dbo].[Deposits] where DepositId = " & txtDepositID.Text, conn)
-                cmd.ExecuteNonQuery()
-                'MessageBox.Show("רשומה התווספה בהצלחה.")
-                conn.Close()
-                GetDepositDetails(False)
-            End If
-        Else
-            MessageBox.Show("שגיאה בהזנת המידע : " & errMsg)
-        End If
-        conn.Close()
-        GetDepositDetails(False)
-    End Sub
-
 
     Private Sub dgTiful_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles dgTiful.CellMouseClick
         If e.RowIndex >= 0 Then
